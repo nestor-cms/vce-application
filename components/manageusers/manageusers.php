e14d7d5bb7732272c821ee4ccc0bd930de59a584
Craig Corvin <corvin@uw.edu>
2019-04-01 11:57:58 -0700

    updates to manage users ans user settings to make nice attribute titles consistant

--- components/manageusers/manageusers.php
+++ components/manageusers/manageusers.php
@@ -232,7 +232,7 @@ EOF;
             foreach ($user_attributes as $user_attributes_key => $user_attributes_value) {
 
                 // nice title for this user attribute
-                $title = isset($user_attributes_value['title']) ? $user_attributes_value['title'] : ucwords(str_replace('_', ' ', $user_attributes_key));
+                $title = isset($user_attributes_value['title']) ? ucwords(str_replace('_', ' ', $user_attributes_value['title'])) : ucwords(str_replace('_', ' ', $user_attributes_key));
 
                 // check if required
                 $tag = (isset($user_attributes_value['required']) && $user_attributes_value['required'] == '1') ? 'required' : null;
@@ -471,7 +471,7 @@ EOF;
                 foreach ($user_attributes as $user_attributes_key => $user_attributes_value) {
 
                     // nice title for this user attribute
-                    $title = isset($user_attributes_value['title']) ? $user_attributes_value['title'] : ucwords(str_replace('_', ' ', $user_attributes_key));
+                    $title = isset($user_attributes_value['title']) ? ucwords(str_replace('_', ' ', $user_attributes_value['title'])) : ucwords(str_replace('_', ' ', $user_attributes_key));
 
                     // check if required
                     $tag = (isset($user_attributes_value['required']) && $user_attributes_value['required'] == '1') ? 'required' : null;
